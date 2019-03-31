38ae8928f90764acabdff45245ca3ceb201f355b
Reyhan <miyabruno21@gmail.com>
2019-03-31 12:33:01 +0700

    Design Layout

--- app/src/main/java/com/reyhan/smartfarm/LoginActivity.java
+++ app/src/main/java/com/reyhan/smartfarm/LoginActivity.java
@@ -1,13 +1,55 @@
 package com.reyhan.smartfarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+
+    Button login;
+    Button register;
+    Button lupapassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.userEditText);
+        password = findViewById(R.id.passEditText);
+
+        login = findViewById(R.id.signinButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginintent = new Intent(view.getContext(), MainActivity.class);
+                view.getContext().startActivity(loginintent);
+            }
+        });
+
+        register = findViewById(R.id.registerButton);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regisintent = new Intent(view.getContext(), RegisterActivity.class);
+                view.getContext().startActivity(regisintent);
+            }
+        });
+
+        lupapassword = findViewById(R.id.lupapasswordButton);
+        lupapassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent recoveryintent = new Intent(view.getContext(), RecoveryActivity.class);
+                view.getContext().startActivity(recoveryintent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/reyhan/smartfarm/RegisterActivity.java
+++ app/src/main/java/com/reyhan/smartfarm/RegisterActivity.java
@@ -1,13 +1,28 @@
 package com.reyhan.smartfarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button buatakun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        buatakun = findViewById(R.id.newaccButton);
+        buatakun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newaccintent = new Intent(view.getContext(), LoginActivity.class);
+                view.getContext().startActivity(newaccintent);
+            }
+        });
+
     }
 }
