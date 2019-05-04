7016873f182406edce65560f4231aa65abf7ea92
Reyhan <miyabruno21@gmail.com>
2019-05-04 22:56:58 +0700

    Commit Rizha

---
+++ app/src/main/java/com/reyhan/smartfarm/ApiInterface.java
@@ -0,0 +1,15 @@
+package com.reyhan.smartfarm;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("top-headlines")
+    Call<News> getNews(
+
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+    );
+
+}
---
+++ app/src/main/java/com/reyhan/smartfarm/NewsApiActivity.java
@@ -0,0 +1,86 @@
+package com.reyhan.smartfarm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class NewsApiActivity extends AppCompatActivity {
+
+    public static final String API_KEY = "40a260b68e69445ca70aadd575ca1be8";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
+    private String TAG = NewsApiActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_api);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(NewsApiActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+        LoadJson();
+    }
+
+    public void LoadJson(){
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+
+        Call<News> call;
+        call = apiInterface.getNews(country, API_KEY);
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if(response.isSuccessful() && response.body().getArticle() != null ){
+
+                    if(!articles.isEmpty()){
+                        articles.clear();
+                    }
+
+                    articles = response.body().getArticle();
+                    adapter = new Adapter(articles, NewsApiActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+
+                } else {
+                    Toast.makeText(NewsApiActivity.this, "No Result", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_news){
+            startActivity(new Intent(NewsApiActivity.this, TimelineActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
